171faed7c690360296375b051c0302eedfa080d5
SageSight3 <sagesightbusiness@gmail.com>
2026-03-13 18:04:01 -0500

    sandboxxed having images in pyside widgets

--- sandbox/PySideSandbox/structure_sandbox/views/ui_mainwindow.py
+++ sandbox/PySideSandbox/structure_sandbox/views/ui_mainwindow.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'mainwindow.ui'
+## Form generated from reading UI file 'mainwindowEzIXka.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.10.2
+## Created by: Qt User Interface Compiler version 6.4.3
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
@@ -16,18 +16,27 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QHBoxLayout, QLabel, QMainWindow,
-    QPushButton, QSizePolicy, QSpacerItem, QVBoxLayout,
-    QWidget)
+    QPushButton, QSizePolicy, QSpacerItem, QToolButton,
+    QVBoxLayout, QWidget)
 
 from appcontainer import AppContainer
+import resources_rc
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
         MainWindow.resize(800, 600)
+        sizePolicy = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
+        MainWindow.setSizePolicy(sizePolicy)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
+        self.centralwidget.setStyleSheet(u"QWidget#centralwidget {\n"
+"	background-image: url(:/images/images/MainWindowBGLowOpacity.png);\n"
+"}")
         self.verticalLayout = QVBoxLayout(self.centralwidget)
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.dashboard = QHBoxLayout()
@@ -37,7 +46,17 @@ class Ui_MainWindow(object):
 
         self.dashboard.addWidget(self.changePageLabel)
 
-        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+        self.arrowButton = QToolButton(self.centralwidget)
+        self.arrowButton.setObjectName(u"arrowButton")
+        self.arrowButton.setStyleSheet(u"")
+        icon = QIcon()
+        icon.addFile(u":/images/images/ArrowButton.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.arrowButton.setIcon(icon)
+        self.arrowButton.setArrowType(Qt.NoArrow)
+
+        self.dashboard.addWidget(self.arrowButton)
+
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
 
         self.dashboard.addItem(self.horizontalSpacer)
 
@@ -46,7 +65,7 @@ class Ui_MainWindow(object):
 
         self.dashboard.addWidget(self.secretMessage)
 
-        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
 
         self.dashboard.addItem(self.horizontalSpacer_2)
 
@@ -71,11 +90,14 @@ class Ui_MainWindow(object):
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
     # setupUi
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
         self.changePageLabel.setText(QCoreApplication.translate("MainWindow", u"Change Page", None))
+        self.arrowButton.setText("")
         self.secretMessage.setText(QCoreApplication.translate("MainWindow", u"Secret Message", None))
         self.prevButton.setText(QCoreApplication.translate("MainWindow", u"Previous", None))
         self.nextButton.setText(QCoreApplication.translate("MainWindow", u"Next", None))
